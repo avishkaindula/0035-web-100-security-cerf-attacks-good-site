e850c5d801fa8563d9fc4ebff5ff6dc66efbb2d4
Avishka Indula <avishkainduladev@gmail.com>
2022-11-05 05:29:13 +0530

    sameSite cookies and lax setting

--- app.js
+++ app.js
@@ -31,6 +31,18 @@ app.use(
     store: sessionStore,
     cookie: {
       maxAge: 2 * 24 * 60 * 60 * 1000,
+      sameSite: "lax",
+      // Same-site cookies is a cookie configuration that can be set on the server side,
+      // where browsers also assume certain default values if it's not explicity configured.
+      // ex: chrome uses lax, but safari and firefox doesn't use lax as default.
+      // Therefor, we need to set this sameSite value in order to make sure that the firefox and safari users
+      // are also using lax. But Browsers like Internet Explorer still can't use lax.
+      // Lax means cookies can be attached to requests that come from a different site but,
+      // only if you visited that site from the main site.
+      // So if we click on a link from an email instead of the main site, then the cookies
+      // would not be attached to the outgoing request. So the post request sent from the "bad site" 
+      // to the localhost:3000/transaction route of the real website won't work.
+      // In localhost, this is disabled. But that doesn't matter because we use localhost only for development.
     },
   })
 );
