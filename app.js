5e9998874b1e03b64bc214c3647a98cfa68d56d0
Avishka Indula <avishkainduladev@gmail.com>
2022-11-05 21:02:51 +0530

    csrf protection with Csurf package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const mongodbStore = require("connect-mongodb-session");
+const csrf = require("csurf");
 
 const db = require("./data/database");
 const demoRoutes = require("./routes/demo");
@@ -40,12 +41,26 @@ app.use(
       // Lax means cookies can be attached to requests that come from a different site but,
       // only if you visited that site from the main site.
       // So if we click on a link from an email instead of the main site, then the cookies
-      // would not be attached to the outgoing request. So the post request sent from the "bad site" 
+      // would not be attached to the outgoing request. So the post request sent from the "bad site"
       // to the localhost:3000/transaction route of the real website won't work.
       // In localhost, this is disabled. But that doesn't matter because we use localhost only for development.
     },
   })
 );
+
+// We need to add the csrf package after app.use(sessions) because csrf use sessions under the hood.
+app.use(csrf());
+// This will create csrf tokens
+// These are random string values that are generated on the server side, which are known by the server and
+// which are short lived. They only exist for one request cycle.
+// These tokens are known to the server but the phishing attacker has no way of knowing this token.
+// These tokens are injected to the templates that are rendered by the server via hidden input fields.
+// And then for incoming requests, the server checks whether such a valid token is part of that request or not.
+// As the phishing attacker has no way of re-creating those tokens, the post requests that are sent by the
+// phishing attacker doesn't have these tokens. Therefor, the post requests send by phishing attackers are ignored.
+// Now we need to register this csrf tokens for all the routes that contain templates with forms.
+// We need to add csrf tokens to any request that manipulate something on the
+// backend (Typically those are post requests) because those are the requests that are most vulnerable to csrf attacks.
 
 app.use(async function (req, res, next) {
   const user = req.session.user;
